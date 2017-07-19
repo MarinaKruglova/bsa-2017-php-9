32eab56b5f39254fdab118e664d88b9bfed88eb0
Marina Kruglova <marina.kruglova@gmail.com>
2017-07-19 10:39:05 +0000

    added migrations on users and cars

---
+++ database/migrations/2017_07_19_093430_create_cars_table.php
@@ -0,0 +1,45 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateCarsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('cars', function (Blueprint $table) {
+            $table->engine = 'InnoDB';
+
+            $table->increments('id');
+            $table->string('color');
+            $table->string('model');
+            $table->string('registration_number');
+            $table->integer('year');
+            $table->integer('mileage');
+            $table->float('price');
+            $table->integer('user_id')->unsigned();
+            $table->timestamps();
+        });
+
+        Schema::table('cars', function (Blueprint $table) {
+            $table->foreign('user_id')->references('id')->on('users');
+        });
+
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('cars');
+    }
+}
